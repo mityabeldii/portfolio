407a40720f8d6c74e9f5c500acbb06f530e1ee1a
Dmytro Beldii <mityabeldii@gmail.com>
2023-11-10 17:27:45 +0100

    Fix avatar

--- apps/web/src/components/pages/home-page.tsx
+++ apps/web/src/components/pages/home-page.tsx
@@ -1,25 +1,25 @@
-const HomePage = () => {
+import avatarImg from 'assets/avatar.jpeg';
+
+export default function HomePage() {
     return (
         <div className="flex fle-row gap-5 items-center justify-center">
             <div className="relative p-2 flex items-center justify-center">
-                <img
-                    src="https://media.licdn.com/dms/image/D4E03AQFxWA2JfEofsw/profile-displayphoto-shrink_800_800/0/1677690594269?e=1691625600&v=beta&t=sdH_mcyeCyOcF7siQmAXeub-xBkS8UQLgX2QoYbtOW0"
-                    className="rounded-full w-24 aspect-square"
-                />
-                <div className="half-circle absolute -bottom-0 top-0 border-ua-blue"></div>
-                <div className="half-circle rotate-180 absolute -bottom-0 border-ua-yellow"></div>
+                <img src={avatarImg} className="rounded-full w-36 aspect-square border-[#39a266] border-4" />
+                {/* <div className="half-circle absolute -bottom-0 top-0 border-ua-blue"></div>
+                <div className="half-circle rotate-180 absolute -bottom-0 border-ua-yellow"></div> */}
             </div>
             <h1 className="font-bold">Dmytro Beldii</h1>
             <div className="absolute bottom-0 mb-5 flex flex-row gap-2">
-                <a href="https://instagram.com/mityabeldii" target="_blank">
+                {/* <a href="https://instagram.com/mityabeldii" target="_blank">
                     <button>instagram</button>
-                </a>
-                <a href="https://linkedin.com/in/dmytro-beldii-8592b1188/" target="_blank">
+                </a> */}
+                <a href="https://linkedin.com/in/mityabeldii" target="_blank">
                     <button>linkedin</button>
+                </a>
+                <a href="https://github.com/mityabeldii" target="_blank">
+                    <button>github</button>
                 </a>
             </div>
         </div>
     );
-};
-
-export default HomePage;
+}
